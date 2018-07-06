5a00da36d5976d701ed510287f5e975ff4840681
ZhouWHao <975865149@qq.com>
2018-07-06 17:33:46 +0800

    更新说明

---
+++ fomat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#Filename: fomat.py
+age = 20
+name="小明"
+i = 50
+s = '''This is a multi-line string.
+7 This is the second line.'''
+print(i+1)
+print(s)
+print("{0}今年{1}岁了。".format(name,age))
+print(name + ' is'+str(age)+' years old')
+
