fb644991ced6a5730031b0b484ffbfb5042ed4be
ZhouWHao <975865149@qq.com>
2018-07-13 15:50:57 +0800

    if控制流语句学习

---
+++ if.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+number= 23;
+guess = int(input('输入一些东西'))
+if guess == number:
+    print('内容相同')
+elif guess<number:
+    print('输入的内容小于number')
+else:
+    print('输入的内容大于number')
+print('Done')
+
+if True:
+    print(12312312321)
